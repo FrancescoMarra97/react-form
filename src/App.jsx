886ea6dfa32a78d9527fc42044d80239e116df72
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-18 16:48:20 +0100

    on submit

--- src/App.jsx
+++ src/App.jsx
@@ -16,12 +16,18 @@ function App() {
         <div>
           <h1 className='mb-4'>Form</h1>
         </div>
-        <form>
+        <form onSubmit={addTitle}>
           <div className="input-group mb-3">
             <input type="text" className="form-control" placeholder="Add new article" aria-label="Recipient's username" aria-describedby="button-addon2" />
             <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
           </div>
+          <small id='titleHelperId' className='form-text text-muted'>type your new title</small>
         </form>
+
+        <ul className='list-group'>
+          {title.map((articleTitle, index) => <li key={index} className='list-group-item'>{articleTitle}</li>)}
+
+        </ul>
       </div>
     </>
   )

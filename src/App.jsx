da7d5602fc1f4da78405fc2bd7efc9c846589fb6
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-18 17:44:26 +0100

    finial commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,10 +22,11 @@ function App() {
   }
 
   function handleTrashTitleClick(e) {
+
     const titleIndexToTrash = Number(e.target.getAttribute("data-index"))
     console.log(titleIndexToTrash);
     const removeTitles = titles.filter((title, index) => index != titleIndexToTrash)
-    console.log(newTitles);
+    console.log(removeTitles);
     setTitle(removeTitles)
   }
   return (
@@ -41,7 +42,7 @@ function App() {
             <input type="text" className="form-control" placeholder="Add new article"
               aria-label="Recipient's username" aria-describedby="button-addon2"
               value={newTitle} onChange={e => setNewTitle(e.target.value)} />
-            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Button</button>
+            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Send</button>
           </div>
           <small id='titleHelperId' className='mb-3 form-text text-muted'>type your new title</small>
         </form>
@@ -50,7 +51,7 @@ function App() {
           {titles.map((articleTitle, index) =>
             <li key={index} className='list-group-item d-flex justify-content-between'>
               {articleTitle}
-              <button onClick={handleTrashTitleClick} data-index={index}>
+              <button className="btn btn-sm btn-danger" onClick={handleTrashTitleClick} data-index={index}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash-fill" viewBox="0 0 16 16">
                   <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5M8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5m3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0" />
                 </svg>

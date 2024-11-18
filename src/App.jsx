01a0639a849910f83f186a984bdb2c93a4b25c56
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-18 16:33:42 +0100

    form

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
+const articleTitle = [
+
+]
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState(articleTitle)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="container">
+
+        <div>
+          <h1 className='mb-4'>Form</h1>
+        </div>
+        <form>
+          <div className="input-group mb-3">
+            <input type="text" className="form-control" placeholder="Add new article" aria-label="Recipient's username" aria-describedby="button-addon2" />
+            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }

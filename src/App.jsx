06462b474142fa74204f7066da6516e59e263636
FrancescoMarra97 <francescomarra192@gmail.com>
2024-11-18 16:54:44 +0100

    variabile d'appoggio

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const articleTitle = [
 ]
 function App() {
   const [title, setTitle] = useState(articleTitle)
-
+  const [newTitle, setNewTitle] = useState("")
 
   function addTitle(e) {
     e.preventDefault()
@@ -24,8 +24,9 @@ function App() {
         </div>
         <form onSubmit={addTitle}>
           <div className="input-group mb-3">
-            <input type="text" className="form-control" placeholder="Add new article" aria-label="Recipient's username" aria-describedby="button-addon2" />
-            <button className="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+            <input type="text" className="form-control" placeholder="Add new article"
+              aria-label="Recipient's username" aria-describedby="button-addon2" value={title} />
+            <button className="btn btn-outline-secondary" type="submit" id="button-addon2">Button</button>
           </div>
           <small id='titleHelperId' className='form-text text-muted'>type your new title</small>
         </form>
